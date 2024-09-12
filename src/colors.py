cae7e829a65e68118f951942b3c3f2433bf5aaeb
Victor Wildner <vwildner@redhat.com>
2024-09-12 15:16:44 -0300

    refactor: update constants module
    - add colors module

---
+++ src/colors.py
@@ -0,0 +1,5 @@
+class Color:
+    YELLOW = "\033[33m"
+    RED = "\033[31m"
+    BOLD = "\033[1m"
+    END = "\033[0m"
--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,4 @@
+from src.utils import find_host_ip
 import os
 
-
-DEFAULT_HOST_IP = "172.22.0.2"
-HOST_IP = os.getenv("HOST_IP", DEFAULT_HOST_IP)
-
-
-class Color:
-    YELLOW = "\033[33m"
-    RED = "\033[31m"
-    BOLD = "\033[1m"
-    END = "\033[0m"
+HOST_IP = os.getenv("HOST_IP", find_host_ip())
